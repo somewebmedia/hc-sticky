893c77c01d9b8b7b8df4f985458c779e29138b26
Some Web Media <somewebmedia@gmail.com>
2018-04-13 14:26:43 +0200

    Added forEach polyfill

--- src/hc-sticky.helpers.js
+++ src/hc-sticky.helpers.js
@@ -33,7 +33,46 @@
     });
   }
 
-  // https://github.com/desandro/eventie
+  /*
+   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+   */
+  if (!Array.prototype.forEach) {
+    Array.prototype.forEach = function(callback) {
+      var T, k;
+
+      if (this == null) {
+        throw new TypeError('this is null or not defined');
+      }
+
+      var O = Object(this);
+      var len = O.length >>> 0;
+
+      if (typeof callback !== 'function') {
+        throw new TypeError(callback + ' is not a function');
+      }
+
+      if (arguments.length > 1) {
+        T = arguments[1];
+      }
+
+      k = 0;
+
+      while (k < len) {
+        var kValue;
+
+        if (k in O) {
+          kValue = O[k];
+          callback.call(T, kValue, k, O);
+        }
+
+        k++;
+      }
+    };
+  }
+
+  /*
+   * https://github.com/desandro/eventie
+   */
   const event = (() => {
     var docElem = document.documentElement;
 
