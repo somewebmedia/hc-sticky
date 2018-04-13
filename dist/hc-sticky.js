f6bd69155cea3a6044db122313c3f5ba1a4d445a
Some Web Media <somewebmedia@gmail.com>
2018-04-13 21:49:59 +0200

    2.1.4

--- dist/hc-sticky.js
+++ dist/hc-sticky.js
@@ -1,7 +1,7 @@
 /*!
  * HC-Sticky
  * =========
- * Version: 2.1.3
+ * Version: 2.1.4
  * Author: Some Web Media
  * Author URL: http://somewebmedia.com
  * Plugin URL: https://github.com/somewebmedia/hc-sticky
