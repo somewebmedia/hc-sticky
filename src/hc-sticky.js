dafdf7ea51790e4b164b8b0c840b30b64d41b268
Some Web Media <somewebmedia@gmail.com>
2018-06-27 15:07:46 +0200

    2.2.0

--- src/hc-sticky.js
+++ src/hc-sticky.js
@@ -1,7 +1,7 @@
 /*!
  * HC-Sticky
  * =========
- * Version: 2.1.7
+ * Version: 2.2.0
  * Author: Some Web Media
  * Author URL: http://somewebmedia.com
  * Plugin URL: https://github.com/somewebmedia/hc-sticky
